924466fa344ab9f79e97db277750cafabb5b5958
MMartindale3 <145407645+MMartindale3@users.noreply.github.com>
2024-04-11 21:16:26 -0600

    Delete lab-7 directory
    Incorrect file uploaded

--- lab-7/js/index.js
+++
@@ -1,7 +0,0 @@
-async function appInit() {
-    const response = await fetch("https://661897109a41b1b3dfbd735b.mockapi.io/albums");
-    const fetchData = await response.json();
-    console.log(fetchData);    
-}
-
-appInit();
